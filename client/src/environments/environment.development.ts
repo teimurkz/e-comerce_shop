3a8418d3fa1cb8b374375fdad6f73b4be5dc4aae
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 01:45:10 +0500

    env changes key 1.5

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51Q7wdG2Ms3EmV33FC2Rm0RTfYo7XhZpbzMKjbHrMElEBzd70WJ8EWfbGPOAQs5qGpPFdDsayYFWiyNBQGJ0UMeLK002BVzRh1q'
+    stripePublicKey: 'pk_test_51QEzQuGTrdwVMF5PAJqHjoRTxrJC2furK61NEJgKXPOU0BYFStRbrC1VJhvSlUBFyc6uXKjnos18i8x0HZr8yawu00tIvup3Fx'
 };
