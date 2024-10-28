0e122d688dc8a92c6c875df8cdcb4ea6546ccd51
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 00:08:25 +0500

    env changes key

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51QEvXsGIfpZsoPCNyA8HzxTyei2dwAUj2CPoUgMRwNfmEZ6DpIxBKdAryTcCAnhebVcX9P5eunreQ8k7pMJGeNXQ00wdmVEv4o'
+    stripePublicKey: 'pk_test_51Q7wdG2Ms3EmV33FC2Rm0RTfYo7XhZpbzMKjbHrMElEBzd70WJ8EWfbGPOAQs5qGpPFdDsayYFWiyNBQGJ0UMeLK002BVzRh1q'
 };
