2556463998b45cdc64dcf01b63a3b63853545631
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 00:36:36 +0500

    env changes key 0.8

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: ''
+    stripePublicKey: 'pk_test_51QEvXsGIfpZsoPCNyA8HzxTyei2dwAUj2CPoUgMRwNfmEZ6DpIxBKdAryTcCAnhebVcX9P5eunreQ8k7pMJGeNXQ00wdmVEv4o'
 };
