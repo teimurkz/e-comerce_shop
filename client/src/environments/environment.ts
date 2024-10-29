b80440162fc2d1c3c1d72b39dd48ae07e2d6b6d1
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 21:31:59 +0500

    test2

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51Q7wdG2Ms3EmV33FC2Rm0RTfYo7XhZpbzMKjbHrMElEBzd70WJ8EWfbGPOAQs5qGpPFdDsayYFWiyNBQGJ0UMeLK002BVzRh1q'
+    stripePublicKey: 'pk_test_51QEzQuGTrdwVMF5PAJqHjoRTxrJC2furK61NEJgKXPOU0BYFStRbrC1VJhvSlUBFyc6uXKjnos18i8x0HZr8yawu00tIvup3Fx'
 };
