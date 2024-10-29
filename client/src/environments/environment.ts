6b2139726d666410c082a1f2384692288157fe09
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 11:01:51 +0500

    test

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: ''
+    stripePublicKey:'pk_test_51QEzQuGTrdwVMF5PAJqHjoRTxrJC2furK61NEJgKXPOU0BYFStRbrC1VJhvSlUBFyc6uXKjnos18i8x0HZr8yawu00tIvup3Fx'
 };
