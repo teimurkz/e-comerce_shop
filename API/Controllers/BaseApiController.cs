f28c8bb6985d46c801a4179963ec0da36de1277a
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-10-29 20:57:57 +0500

    test

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -7,16 +7,15 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController: ControllerBase
+    public class BaseApiController : ControllerBase
     {
-        protected async Task<ActionResult> CreatePageResult <T>(IGenericRepository<T> repo, 
-        ISpecefication<T> spec ,int pageIndex , int pageSize) where T : BaseEntity
+        protected async Task<ActionResult> CreatePageResult<T>(IGenericRepository<T> repo,
+        ISpecefication<T> spec, int pageIndex, int pageSize) where T : BaseEntity
         {
             var items = await repo.ListAsync(spec);
             var count = await repo.CountAsync(spec);
-             var pagination = new Pagination<T>(pageIndex,pageSize,count,items);
-
-             return Ok(pagination);
+            var pagination = new Pagination<T>(pageIndex, pageSize, count, items);
+            return Ok(pagination);
         }
     }
 }
