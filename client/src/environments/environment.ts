078bb2af27b8e96b22c8102240697ba176a741cf
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-11-01 14:44:12 +0500

    pre continuous integration

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51Q7wdG2Ms3EmV33FC2Rm0RTfYo7XhZpbzMKjbHrMElEBzd70WJ8EWfbGPOAQs5qGpPFdDsayYFWiyNBQGJ0UMeLK002BVzRh1q'
+    stripePublicKey: 'pk_test_51QGGBsAiDWtzqRWPchm6rv7LMcLlDlPZHqJtrXMb8kFItWHHBuhpGUWy88NnuEmr913Arko7p5mCZ9IZy68YLHuX00USmA0kHT'
 };
