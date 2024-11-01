8a9ccc40b7be8889a200c8dbd317d6137e3e1a79
MacOs Teimur <teimurfarajov@MBP-Teimur.(none)>
2024-11-01 17:51:21 +0500

    key test

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
     hubUrl: 'https://localhost:5001/hub/notifications',
-    stripePublicKey: 'pk_test_51Q7wdG2Ms3EmV33FC2Rm0RTfYo7XhZpbzMKjbHrMElEBzd70WJ8EWfbGPOAQs5qGpPFdDsayYFWiyNBQGJ0UMeLK002BVzRh1q'
+    stripePublicKey: 'pk_test_51QGGBsAiDWtzqRWPchm6rv7LMcLlDlPZHqJtrXMb8kFItWHHBuhpGUWy88NnuEmr913Arko7p5mCZ9IZy68YLHuX00USmA0kHT'
 };
